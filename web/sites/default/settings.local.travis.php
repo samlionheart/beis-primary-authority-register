cb643fc75f1573d02ca704aff4026c4d0569b3b4
Chris Moreton <chris@netsensia.com>
2017-11-07 22:22:19 +0000

    Increase memory limit and timeout

--- web/sites/default/settings.local.travis.php
+++ web/sites/default/settings.local.travis.php
@@ -1,8 +1,8 @@
 <?php
 
 // Ensure travis always runs with the same memory that other environments do.
-ini_set('memory_limit', '512M');
-ini_set('max_execution_time', 900);
+ini_set('memory_limit', '2048M');
+ini_set('max_execution_time', 1000);
 
 $databases['default']['default'] = array (
   'database' => 'par',
