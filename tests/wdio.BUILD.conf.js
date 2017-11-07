43d597a61cbea0c30f45183a20f5a46bb88532ff
Chris Moreton <chris@netsensia.com>
2017-11-07 15:25:04 +0000

    Try pausing before starting tests

--- tests/wdio.BUILD.conf.js
+++ tests/wdio.BUILD.conf.js
@@ -13,4 +13,25 @@ config.baseUrl = 'http://127.0.0.1:80';
 config.tags = '@ci, ~@Pending, ~@setup, ~@deprecated, ~@Bug';
 config.cucumberOpts.failFast = true;
 config.bail = 1;
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+config.before = function before() {
+	
+	console.log('Pausing before starting tests...');
+	await sleep(12000);
+	
+    /**
+     * Setup the Chai assertion framework
+     */
+    const chai = require('chai');
+
+    global.expect = chai.expect;
+    global.assert = chai.assert;
+    global.should = chai.should();
+}
+
 exports.config = config;
+
