532a9a5f8151d8d36decda14f99594e8d6482977
Chris Moreton <chris@netsensia.com>
2017-12-09 19:56:35 +0000

    Apply new Vagrant box and helper script

---
+++ vagrant.sh
@@ -0,0 +1,18 @@
+# Run this command in the directory above the repo and Have a copy of your .env file in that directory
+#
+# sh beis-primary-authority-register/vagrant.sh
+#
+
+cd beis-primary-authority-register
+vagrant destroy
+cd ..
+sudo lsof -ti:8111 | xargs kill
+rm -rf beis-primary-authority-register
+git clone git@github.com:UKGovernmentBEIS/beis-primary-authority-register
+cd beis-primary-authority-register
+cp ../.env .
+php composer.phar install
+chmod -R 777 web/modules/contrib
+vagrant up
+
+
